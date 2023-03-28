f96050c2a49ff8b32992352f94096e154f0cf758
Aditya Bikram Arandhara <adityabikrambill@gmail.com>
2023-03-28 20:19:14 +0530

    Added AI Mouse code

--- main.py
+++ main.py
@@ -20,19 +20,31 @@ cap = cv2.VideoCapture(0)
 
 #Hand Detection
 detector = HandDetector(detectionCon=0.8, maxHands=1)
+hand_detector = mp.solutions.hands.Hands()
 
 #Mediapipe hands
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
+#Drawing Utils
+drawing_utils = mp.solutions.drawing_utils
+
+screenHeight, screenWidth = pyautogui.size()
+index_y = 0
+
 while True:
     success, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    rgbFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    output = hand_detector.process(rgbFrame)
 
     #imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     hands, frame = detector.findHands(frame)
     cv2.line(frame, (0, gestureThreshold), (width, gestureThreshold), (0, 255, 0), 10)
+
+    windowHeight, windowWidth, _ = frame.shape
 
     if hands and buttonPressed is False:
         hand = hands[0]
@@ -40,7 +52,7 @@ while True:
         cx, cy = hand['center']
         lmList = hand['lmList']
 
-        if cy <= gestureThreshold:
+        if cy <= gestureThreshold: #Multimedia and Presentation Control(next and previous)
            if fingers == [1, 0, 0, 0, 0]: #Gesture - left
                pyautogui.press("left")
                buttonPressed = True
@@ -49,17 +61,37 @@ while True:
                pyautogui.press("right")
                buttonPressed = True
 
-        if fingers == [1, 1, 1, 1, 1]:
+        if fingers == [1, 1, 1, 1, 1]: #Stop and Play
             pyautogui.press("space")
             buttonPressed = True
 
-        if fingers == [0, 1, 0, 0, 1]:
+        if fingers == [0, 1, 0, 0, 1]: #Brightness Increase
             volume = volume+5
             sbc.set_brightness(volume)
 
-        if fingers == [1, 0, 0, 0, 1]:
+        if fingers == [1, 0, 0, 0, 1]: #Brightness Decrease
             volume = volume-5
             sbc.set_brightness(volume)
+
+        if fingers == [0, 1, 1, 0, 0]: #AI Virtual Mouse
+            hands = output.multi_hand_landmarks
+            for hand in hands:
+                drawing_utils.draw_landmarks(frame, hand)
+                landmarks = hand.landmark
+                for id, landmark in enumerate(landmarks):
+                    x = int(landmark.x * windowWidth)
+                    y = int(landmark.y * windowHeight)
+                    if id == 8:
+                        cv2.circle(img=frame, center=(x, y), radius=10, color=(0, 255, 255))
+                        index_x = screenWidth / windowWidth * x
+                        index_y = screenHeight / windowHeight * y
+                        pyautogui.moveTo(index_x, index_y)
+                    if id == 12:
+                        cv2.circle(img=frame, center=(x, y), radius=10, color=(0, 255, 255))
+                        thumb_x = screenWidth / windowWidth * x
+                        thumb_y = screenHeight / windowHeight * y
+                        if (abs(thumb_x - index_x) < 70):
+                            pyautogui.click()
 
 
 
